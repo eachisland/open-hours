0e7f16c06adf390955f993eeb1944ac9a0388344
eachisland <126067130+eachisland@users.noreply.github.com>
2023-03-01 13:23:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Reflection.PortableExecutable;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+           double h = double.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "Monday":
+                    if(h >=10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Tuesday":
+                    if (h >= 10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Wensday":
+                    if (h >= 10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Thursday":
+                    if (h >= 10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Friday":
+                    if (h >= 10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Saturday":
+                    if (h >= 10 && h <= 18)
+                    {
+                        Console.WriteLine("open");
+                    }
+                    else
+                    {
+                        Console.WriteLine("closed");
+                    }
+                    break;
+                case "Sunday":
+                      Console.WriteLine("closed");
+                    break;
+            }
+        }
+    }
+}
